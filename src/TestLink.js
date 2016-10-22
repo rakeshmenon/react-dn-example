b430aec6ff59a9978bcc2e34ebbf0b432775e391
Rakesh Menon <rmenon3@sapient.com>
2016-10-22 09:42:38 +0530

    Initial commit

---
+++ src/TestLink.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+class Test extends Component {
+  render() {
+    return (
+      <Link to="/test">Test</Link>
+    );
+  }
+}
+
+export default Test;
